1e82b0fed114a582d7ddd7d69becef1cd61c8cc8
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-03-10 12:08:45 +0530

    ehe

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,21 @@ import {motion} from 'framer-motion'
 
 const Card = ({image,sumTitle, sumPara, buttonText }) => {
   return (
-    <motion.div className='card-container overflow-hidden  w-[90%] border md:w-[30%] shadow-2xl shadow-black mt-12 rounded-2xl  mx-auto'>
-     <motion.div className="img-container  w-[50%] mx-auto py-8">
-        <img className='w-24 h-24 ml-12' src={image} alt="" />
-     </motion.div>
-     <motion.div className="summary w-[320px]  ">
-        <h2 className='text-2xl  2xl:text-4xl text-white ml-32 font-bold '>{sumTitle}</h2>
-        <p className='text-lg lg:text-base lg:ml-3  ml-12 w-96 pr-12 mt-4 text-slate-200'>{sumPara}</p>
-     </motion.div>
-     <motion.div className="button  w-[60%]   mx-10 mt-8 ">
-        <button className='px-1 py-2 lg:px-1  lg:py-1 ml-24 2xl:px-2 2xl:py-2 bg-white font-semibold mb-8  rounded-xl transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-250  duration-300 hover:bg-white hover:border-2 hover:text-black hover:border-green-700 '>{buttonText}</button>
-     </motion.div>
-    
-</motion.div>
+    <motion.div className='card-container  max-w-md border-1 shadow-lg shadow-black border-white rounded-lg mt-4' >
+        <motion.div className='img-container p-4 ml-40 w-full'>
+            <img className='w-24 h-24 ' src={image} alt="" />
+        </motion.div>
+        <motion.div className='sum-title p-4 w-full ml-32'>
+            <h2 className=' text-white text-3xl font-semibold'>{sumTitle}</h2>
+        </motion.div>
+        <motion.div className='sum-para p-4 w-3/4 ml-20'>
+            <p className=' text-white  '>{sumPara}</p>
+        </motion.div>
+        <motion.div className='sum-btn p-4 ml-40 '>
+            <button className=' bg-white px-3 py-2 rounded-xl font-semibold hover:transition-all hover:border-2 hover:border-green-700'>{buttonText}</button>
+        </motion.div>
+
+         </motion.div>
   )
 }
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import {motion} from 'framer-motion'
 import collabImg from '../assets/collab.svg'
 
@@ -6,26 +7,28 @@ import Card from './Card'
 
 const Services = () => {
   return (
-    <motion.div className=' flex flex-shrink  flex-wrap  md:gap-3 w-[90%] h-auto bg- mx-auto rounded-xl my-4'>
-    <Card 
-        image={collabImg}
-        sumTitle="Collaborate"
-        sumPara="Work collaboratively with supportive designers and create your perfect website"
-        buttonText="Check our work"
-    />
-    <Card 
-        image={collabImg}
-        sumTitle="Collaborate"
-        sumPara="Work collaboratively with supportive designers and create your perfect website"
-        buttonText="Check our work"
-    />
-    <Card 
-        image={collabImg}
-        sumTitle="Collaborate"
-        sumPara="Work collaboratively with supportive designers and create your perfect website"
-        buttonText="Check our work"
-    />
-    </motion.div>
+         <motion.div className='w-[90%]  h-auto flex flex-wrap mx-auto overflow-hidden gap-3  justify-center items-center'>
+         <Card
+         image={collabImg}
+         sumTitle="Collaborate"
+         sumPara="Work collaboratively with supportive designers and create your perfect website."
+         buttonText="Check out"
+           />
+         <Card
+         image={collabImg}
+         sumTitle="Collaborate"
+         sumPara="Work collaboratively with supportive designers and create your perfect website."
+         buttonText="Check out"
+           />
+         <Card
+         image={collabImg}
+         sumTitle="Collaborate"
+         sumPara="Work collaboratively with supportive designers and create your perfect website."
+         buttonText="Check out"
+           />
+        
+
+         </motion.div>
   )
 }
 
