1a90531799631995064bae2610e3947c5ace4499
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-03-17 16:21:46 +0530

    ehe

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className='w-[90%] rounded-xl  h-auto mt-20 flex flex-col items-center'>
         <div className="curious pl-10 py-20 ml-20 ">
             <h1 className='text-white text-[2.6rem] font-extrabold md:text-[3.5rem] md:ml-16'>Curious how we do it?</h1>
-            <button className='px-24 py-3 rounded-full mt-8  bg-white md:px-36 md:ml-40 ' >Contact us</button>
+            <button className='px-24 text-xl py-3 rounded-full mt-8 font-semibold bg-white md:px-36 md:ml-40 transition ease-in-out delay-150  hover:bg-gray-700 hover:text-white hover:border-2 hover:border-green-700' >Contact us</button>
         </div>
     </div>
   )
