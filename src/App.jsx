623440961a4af95c9468be8544aeebb424093b41
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-03-20 16:27:00 +0530

    ehe

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero"
 import Services from "./components/Services"
 import Heading from "./components/Heading"
 import HireUs from "./components/HireUs"
-import Contact from "./components/Contact"
+
 import Foooter from './components/Foooter'
 
 
