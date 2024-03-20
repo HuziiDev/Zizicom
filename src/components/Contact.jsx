1ad45588b0e133c5618c864c405eafbd4ac98dfa
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-03-20 15:38:50 +0530

    ehe

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-
+import { useEffect } from 'react'
+import {motion , useMotionTemplate, useMotionValue,animate} from 'framer-motion'
+const COLORS=['#13FFAA', '#1E67C6', '#CE84CF', '#DD335C']
 const Contact = () => {
+  const colors= useMotionValue(COLORS[0])
+  const backgroundImage= useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${colors})`
+  const border = useMotionTemplate`1px solid ${colors}`
+  const boxShadow= useMotionTemplate`0px 4px 24px ${colors}`
+  useEffect(() => {
+    animate(colors, COLORS,{
+      ease: 'easeInOut',
+      duration:10,
+      repeat:Infinity,
+      repeatType:"mirror"
+    })
+  },[])
   return (
-    <div className='w-[90%] rounded-xl  h-auto mt-20 flex flex-col items-center'>
-        <div className="curious pl-10 py-20 ml-20 ">
+    <motion.div className='relative w-[90%] rounded-xl ml-16  h-auto mt-20 flex flex-col items-center'
+   
+     style={{backgroundImage}}>
+        <motion.div className="curious pl-10 py-64  " >
             <h1 className='text-white text-[2.6rem] font-extrabold md:text-[3.5rem] md:ml-16'>Curious how we do it?</h1>
-            <button className='px-24 text-xl py-3 rounded-full mt-8 font-semibold bg-white md:px-36 md:ml-40 transition ease-in-out delay-150  hover:bg-gray-700 hover:text-white hover:border-2 hover:border-green-700' >Contact us</button>
-        </div>
-    </div>
+            <motion.button className='absolute px-24 text-white text-xl py-3 rounded-full mt-8 font-semibold bg-transparent md:px-36 md:ml-40' style={{border,boxShadow}}
+             whileHover={{scale:1.015}}
+            whileTap={{
+              scale:0.985
+              }}  
+     >Contact us
+     </motion.button>
+        </motion.div>
+    </motion.div>
   )
 }
 
--- src/components/Foooter.jsx
+++ src/components/Foooter.jsx
@@ -1,29 +1,49 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { motion, useMotionTemplate, useMotionValue ,animate} from 'framer-motion';
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa6";
-
+const COLORS=['#13FFAA', '#1E67C6', '#CE84CF', '#DD335C']
 const Footer = () => {
-    const style = { color: "white", fontSize: "3rem",  }
-  return (
-    <div className='footer-container  w-full h-auto mt-40 bg-black'>
-    <div className='outer w-full bg-green-700 h-1 mt-20 '></div>
-    <div className="main-f-container flex flex-col md:flex md:flex-row md:justify-between pt-20 pb-20">
+  const colors= useMotionValue(COLORS[0])
+  const backgroundImage= useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${colors})`
+  
 
-     <div className="copyright ml-16 md:ml-28">
+
+
+
+    const style = { color: "white", fontSize: "3rem",  }
+
+    useEffect(() => {
+      animate(colors, COLORS,{
+        ease: 'easeInOut',
+        duration:10,
+        repeat:Infinity,
+        repeatType:"mirror"
+      })
+    },[])
+  return (
+    <motion.div className='footer-container  w-full h-auto mt-40 '
+    style={{backgroundImage}}
+    >
+    <motion.div className='outer w-full bg-green-700 h-1 mt-20 '></motion.div>
+    <motion.div className="main-f-container flex flex-col md:flex md:flex-row md:justify-between pt-20 pb-20">
+
+     <motion.div className="copyright ml-16 md:ml-28">
      <h1 className=' text-zinc-600 text-2xl md:text-4xl'> &copy; Agency Name All rights reserved</h1>
-     </div>
+     </motion.div>
        
        
-        <div className="socials flex mr-28 ml-32 mt-20 md:mt-0 gap-8">
+        <motion.div className="socials flex mr-28 ml-32 mt-20 md:mt-0 gap-8">
            
             <FaSquareFacebook style={style} className='icon ' />
            <FaXTwitter style={style} className='icon' />
            <FaLinkedinIn style={style} className='icon' />
         
-        </div>
-        </div>
-    </div>
+        </motion.div>
+        </motion.div>
+    </motion.div>
   )
 }
 
