5160840c51f2d349b50f9309d7926b90a5938366
HuziiDev <123925938+HuziiDev@users.noreply.github.com>
2024-03-23 15:16:45 +0530

    almost

--- src/components/OurServiceCard.jsx
+++ src/components/OurServiceCard.jsx
@@ -1,21 +1,52 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import { GoPencil } from "react-icons/go";
-const OurServiceCard = ({title , content, btn_text}) => {
-    const style = { color: "white", fontSize: "2.5rem",  }
+const OurServiceCard = ({ title, content, btn_text }) => {
+
+  const variants = {
+    initial: {
+      opacity: 0,
+      y: 100
+    },
+    animate: {
+      opacity: 1,
+      y: 0
+    }
+  }
+
+
+
+  const style = { color: "white", fontSize: "2.5rem", }
   return (
-    <div className="first-card w-full bg-zinc-900 border-[0.1px] hover:bg-gradient-to-r from-slate-900 to-slate-700  border-zinc-700 rounded md:w-[30%] ">
+    <motion.div className="first-card w-full bg-gradient-to-r sm:mx-auto from-slate-900 to-slate-700  border-zinc-700 border-[0.1px]  rounded-lg lg:w-[30%] xl:w-[30%] md:w-[40%] sm:w-[70%] shadow-md shadow-white"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+      viewport={{ once: true }}
+      transition={{ ease: 'easeInOut', duration: 1 }}
 
-    <div className="card-content mx-8 my-7">
-    <span> <GoPencil style={style} /> </span>
-    <h2 className='text-[1.3rem] mt-4 text-white font-bold'>{title}</h2>
-    <p className='text-sm mt-4 text-gray-500'>{content}</p>
-    <button className='font-semibold mt-8 text-white  text-xl bg-transparent rounded-3xl px-4 py-1 border-[0.1px] border-zinc-700 hover:bg-white hover:text-black '>{btn_text} <span className='text-xl'>→</span>   </button>
 
-   
-    </div>
-    <div className='w-full h-1 bg-white'></div>
 
-    </div>
+    >
+
+      <motion.div className="card-content mx-8 my-7">
+        <span> <GoPencil style={style} /> </span>
+        <h2 className='text-[1.3rem] mt-4 text-white font-bold'>{title}</h2>
+        <p className='text-sm mt-4 text-gray-500'>{content}</p>
+        <motion.button className='font-semibold mt-8 text-white  text-xl bg-transparent rounded-3xl px-4 py-1 border-[0.1px] border-zinc-700 hover:bg-white hover:text-black '
+          whileHover={{ scale: 1.2 }}
+          transition={{ ease: 'easeInOut', duration: 0.2 }}
+          whileTap={{ scale: 0.9 }}
+
+
+
+        >{btn_text} <span className='text-xl'>→</span>   </motion.button>
+
+
+      </motion.div>
+      <motion.div className='w-full h-1 bg-white'></motion.div>
+
+    </motion.div>
   )
 }
 
--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 import OurServiceCard from './OurServiceCard';
 
 const OurServices = () => {
+   const variants={
+      initial:{
+        opacity:0,
+        y:100
+      },
+      animate:{
+        opacity:1,
+        y:0
+      }
+     }
     
   return (
     <div className='w-[80%] h-auto  mt-36 mx-auto '>
-        <div className="service-heading mx-44">
+        <motion.div className="service-heading 2xl:ml-[3.8rem] xl:ml-[3rem] lg:ml-[2.8rem] md:ml-[2.8rem] sm:ml-[6rem]"
+          variants={variants}
+     initial="initial"
+     whileInView="animate"
+     viewport={{once:true}}
+     transition={{ease:'easeInOut', duration:1}}
+        
+        >
             <h1 className='text-5xl text-gray-100 font-bold'>Services</h1>
             <h3 className='text-lg text-gray-500 mt-2'>Our services for clients</h3>
-        </div>
-        <div className="card-service flex flex-col md:flex-row flex-wrap  w-[85%] gap-2 mx-auto my-24 ">
+        </motion.div>
+        <div className="card-service flex flex-col md:flex-row flex-wrap justify-center  w-[95%] gap-2 mx-auto my-24 ">
          <OurServiceCard
             title='Responsive Design'
             content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, dolorem expedita nemo.'
